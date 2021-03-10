cd26fcea27256ebd63e59646bac0092910e8f834
pycai8 <545152168@qq.com>
2021-03-10 23:23:26 +0800

    add some code

--- Rtsp/CRtspHandler.cpp
+++ Rtsp/CRtspHandler.cpp
@@ -37,8 +37,24 @@ public:
         return "";
     }
 
-    bool SetConfig(const char*, const char*) override
+    bool SetConfig(const char* key, const char* value) override
     {
+		if (std::string(key) == "local.ip") {
+			localIp_ = value;
+			return true;
+		}
+		if (std::string(key) == "peer.ip") {
+			peerIp_ = value;
+			return true;
+		}
+		if (std::string(key) == "local.port") {
+			rtspLocalPort_ = (int)(unsigned long)value;
+			return true;
+		}
+		if (std::string(key) == "peer.port") {
+			rtspPeerPort_ = (int)(unsigned long)value;
+			return true;
+		}
         return false;
     }
 
@@ -52,14 +68,38 @@ public:
         std:string reqType = GetRequestType(req);
         if (req == "OPTIONS") return HandleOptions(req);
         if (req == "DESCRIBE") return HandleOptions(req);
-	if (req == "SETUP") return HandleSetup(req);
-	if (req == "PLAY") return HandlePlay(req);
-	if (req == "TEARDOWN") return HandleTeardown(req);
-	PYCAI_ERROR("request[%s] not support.", reqType.c_str());
-	return "";
+		if (req == "SETUP") return HandleSetup(req);
+		if (req == "PLAY") return HandlePlay(req);
+		if (req == "TEARDOWN") return HandleTeardown(req);
+		PYCAI_ERROR("request[%s] not support.", reqType.c_str());
+		return "";
     }
 
 private:
+	std::string GetRequestType(const std::string& req)
+	{
+		auto pos = req.find(" rtsp://");
+		if (pos == std::string::npos) {
+			return "";
+		}
+
+		return req.substr(0, pos);
+	}
+
+    int GetRequestSequence(const std::string& req)
+	{
+		auto pos = req.find("CSeq: ");
+		if (pos == std::string::npos) {
+			return 0;
+		}
+		int ret = 0;
+		for (auto i = pos; i < req.size() && req[i] != '\r' && req[i] != '\n'; ++i) {
+			if (!('0'<= req[i] && req[i] <= '9')) continue;
+			ret = ret * 10 + req[i] - '0';
+		}
+		return ret;
+	}
+
     std::string localIp_;
     std::string peerIp_;
     int rtspLocalPort_ = 0;
