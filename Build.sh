96679451735909e6d1622e214719ec97d767a008
pycai <545152168@qq.com>
2021-06-26 18:21:24 +0800

    delete file libxxx.so of ffmpeg

--- Build.sh
+++ Build.sh
@@ -5,6 +5,22 @@ cd Bin
 
 cp ../Test/test.h264 . -rf
 
+cp ../3rd/ffmpeg/libavcodec.so.58  . -rf
+cp ../3rd/ffmpeg/libavdevice.so.58  . -rf
+cp ../3rd/ffmpeg/libavfilter.so.7  . -rf
+cp ../3rd/ffmpeg/libavformat.so.58  . -rf
+cp ../3rd/ffmpeg/libavutil.so.56  . -rf
+cp ../3rd/ffmpeg/libswresample.so.3  . -rf
+cp ../3rd/ffmpeg/libswscale.so.5  . -rf
+
+cp ../3rd/ffmpeg/libavcodec.so.58  libavcodec.so -rf
+cp ../3rd/ffmpeg/libavdevice.so.58  libavdevice.so -rf
+cp ../3rd/ffmpeg/libavfilter.so.7  libavfilter.so -rf
+cp ../3rd/ffmpeg/libavformat.so.58  libavformat.so -rf
+cp ../3rd/ffmpeg/libavutil.so.56  libavutil.so -rf
+cp ../3rd/ffmpeg/libswresample.so.3  libswresample.so -rf
+cp ../3rd/ffmpeg/libswscale.so.5  libswscale.so -rf
+
 cd ../Component
 sh Build.sh
 
